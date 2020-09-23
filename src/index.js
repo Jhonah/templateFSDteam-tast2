e3e6805b4bd39ff6db0ed20d0467d76a84615cde
Jhonah <buryakone@gmail.com>
2020-09-24 01:37:33 +0700

    попытка настроить htmlwebpack в ./src/index.html, также extract-text-webpack-plugin не пошел, подключил mini-css-extract-plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+//import "./style.css";
+// SCSS
+import "./scss/main.scss";
 console.log("hello, world");
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 // webpack 4
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: '[name].[hash].js'
     },
     module: {
         rules: [
@@ -14,7 +17,24 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
         ]
-    }
-}
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css',
+        }),
+        new HtmlWebpackPlugin({
+            //inject: false,
+            hash: true,
+            template: './src/index.html',
+            filename: 'index.html',
+            //chunks: ["main"]
+        }),
+        new WebpackMd5Hash()
+    ]
+};
